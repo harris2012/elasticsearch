9c1850a0c2e12c12d9c0c77ab286c3715fd16e14
harriszhang <harriszhang@live.cn>
2019-10-31 22:01:29 +0800

    update

---
+++ Example/Product.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure;
+using System;
+
+namespace Example
+{
+    /// <summary>
+    /// 产品
+    /// </summary>
+    [Index("product")]
+    public class Product
+    {
+        /// <summary>
+        /// 产品Id
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// 产品名称
+        /// </summary>
+        [TextField]
+        public string Name { get; set; }
+    }
+}
